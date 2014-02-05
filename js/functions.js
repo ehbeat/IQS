c7c42016932d81abed546afec794cc8a925dd3ef
Reme Le Hane <info@remelehane.co.za>
2014-02-05 14:14:04 +0200

    basic structure

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+//IE 10 fix for mobiles
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
